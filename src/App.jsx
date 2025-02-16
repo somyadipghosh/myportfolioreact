59ee36b534dad1e35984b787856a5b2b21137b42
somyadipghosh <somyadipghosh@gmail.com>
2025-02-16 18:12:11 +0530

    implement scroll-based visibility toggle for up arrow; enhance styles for smooth transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
@@ -8,7 +8,25 @@ import { Projects } from './components/Projects/Projects'
 import { Contact } from './components/Contact/Contact'
 import { getImageUrl } from './utils'
 
+
+
 const App = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+      const toggleVisibility = () => {
+          if (window.scrollY > 100) {  
+              setIsVisible(true);
+          } else {
+              setIsVisible(false);
+          }
+      };
+
+      window.addEventListener("scroll", toggleVisibility);
+      return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
     <div className={styles.App}>
       <Navbar />
@@ -18,7 +36,7 @@ const App = () => {
       <Projects />
       <Contact />
 
-      <a href="#"><img src={getImageUrl("otherimgs/uparrow.png")} alt="uparrow" className={styles.UpArrow}/></a>
+      <a href="#"><img src={getImageUrl("otherimgs/uparrow.png")} alt="uparrow" className={styles.UpArrow} style={{ opacity: isVisible ? 1 : 0, pointerEvents: isVisible ? "auto" : "none" }}/></a>
     </div>
   )
 }
